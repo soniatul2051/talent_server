59d2648b6cc903d01231afaec9d123361b682050
soniatul2051 <soniatul2051@gmail.com>
2025-11-13 17:11:00 +0530

    fix cors issue

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000'], 
+  credentials: true, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 app.use('/api/talents', talentRoutes);
